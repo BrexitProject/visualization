0796198f5775f317e0bfcfe6d78fe07637e4c74f
haohaoe <820267577@qq.com>
2018-12-23 14:30:12 +0800

    add some tips

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ git clone git@github.com:BrexitProject/visualization.git
 npm install //安装依赖包
 
 npm start //启动项目
-
 ========================================================
+
+开发:
+  在index.js中添加路由,然后在views中新建html进行开发。数据统一存放在public/data下。
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.get('/', function(req, res) {
+app.get('/bubble', function(req, res) {
   res.sendFile(path.join(__dirname + '/views/2_hashtag.html'));
 });
 
